9a245295643243d5154bbaba0b933eca0745955c
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-05 15:19:54 +0100

    feat: Give title to Header of Guess a Number & color the Header pink

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import Header from './components/Header';
+
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View>
-      <Header />
+      <Header title={"Guess a Number"} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import DeprecatedViewPropTypes from "react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <View>
-      <Text>This is the header</Text>
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
@@ -12,5 +13,17 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
-    
-})
+  header: {
+    width: "100%",
+    height: 90,
+    padding: 36,
+    backgroundColor: "#f7278b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
