f33d631bd79a4fbcd926fb6c3a04aee4eaa353fd
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-05 17:23:46 +0100

    feat: create Card component for basic styling

---
+++ components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    backgroundColor: "white",
+    elevation: 5,
+    height: 140,
+    padding: 20,
+    borderRadius: 10,
+  },
+});
