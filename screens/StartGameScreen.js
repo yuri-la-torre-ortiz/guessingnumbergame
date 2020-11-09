6af88689a547578cfd7daf71b3397c276627b6b4
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-09 12:06:20 +0100

    feat: limit input field to numerical values only

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 import Card from "../components/Card";
@@ -6,6 +6,12 @@ import Colors from "../constants/colors";
 import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
@@ -15,8 +21,10 @@ const StartGameScreen = (props) => {
           style={styles.input}
           blurOnSubmit
           autoCorrect={false}
-          keyboardType="numeric"
+          keyboardType="number-pad"
           maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enteredValue}
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
