ad125d713c0cd90ad49e0dafaec841925ecbc03a
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-09 11:30:36 +0100

    style: give Input box a width of 50 pixels

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,8 +2,8 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 import Card from "../components/Card";
-import Colors from '../constants/colors';
-import Input from '../components/Input';
+import Colors from "../constants/colors";
+import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
   return (
@@ -11,10 +11,10 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a number:</Text>
-        <Input />
+        <Input style={styles.input} />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Reset" onPress={() => {}} color={Colors.secondary}/>
+            <Button title="Reset" onPress={() => {}} color={Colors.secondary} />
           </View>
           <View style={styles.button}>
             <Button title="Confirm" onPress={() => {}} />
@@ -44,6 +44,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10,
+  },
+  input: {
+    width: 50,
   },
   inputContainer: {
     width: 300,
