f9a402fa5ad18a33343409b9a1131caa6458ad18
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-09 15:29:21 +0100

    feat: dismiss keyboard if space outside of input box is tapped

--- App.js
+++ App.js
@@ -3,15 +3,20 @@ import { StyleSheet, View } from "react-native";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import DismissKeyboard from "./components/DismissKeyboard";
 
-export default function App() {
+const App = (props) => {
   return (
-    <View>
-      <Header title={"Guess a Number"} />
-      <StartGameScreen />
-    </View>
+    <DismissKeyboard>
+      <View {...props} style={styles.screen}>
+        <Header title={"Guess a Number"} />
+        <StartGameScreen />
+      </View>
+    </DismissKeyboard>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   screen: {
