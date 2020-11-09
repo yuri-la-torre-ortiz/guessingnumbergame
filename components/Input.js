4900e40240925ff896b1d5ef05529d4b9e5260a4
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-09 11:26:06 +0100

    refactor: create Input box component with gray bottom border

---
+++ components/Input.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const Input = (props) => {
+  return <TextInput style={{ ...styles.input, ...props.style }} />;
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 30,
+    borderColor: "white",
+    borderBottomColor: "grey",
+    borderWidth: 1,
+    marginVertical: 10,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
-import Card from "../components/Card"; 
-
+import Card from "../components/Card";
 import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = (props) => {
   return (
@@ -11,7 +11,7 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a number:</Text>
-        <TextInput />
+        <Input />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" onPress={() => {}} color={Colors.secondary}/>
