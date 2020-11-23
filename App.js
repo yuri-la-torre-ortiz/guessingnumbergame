bcaa8b0690fb4772aa8833721204fd8364506500
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-11-23 17:56:27 +0100

    feat: import fonts and render AppLoading while waiting for fonts

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -7,9 +9,21 @@ import DismissKeyboard from "./components/DismissKeyboard";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 const App = (props) => {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading />;
+  }
 
   const newGameHandler = () => {
     setGuessRounds(0);
