4dfa91b56abcb3b38f58e1dcc320fa9070d2d094
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-12-16 13:06:00 +0100

    refactor: using TitleText component instead of style variable

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import Colors from "../constants/colors";
+import TitleText from './TitleText';
 
 const Header = (props) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
 };
@@ -21,11 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
-  },
-  headerTitle: {
-    color: "black",
-    fontSize: 18,
-    fontWeight: "bold",
-    fontFamily: "open-sans-bold",
   },
 });
