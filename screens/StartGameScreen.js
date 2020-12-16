92d5482aeb2bacef59034c44066b3345c984e47b
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-12-16 16:22:51 +0100

    refactor: use TitleText instead of styles variable for font

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import Colors from "../constants/colors";
 import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const StartGameScreen = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -58,7 +59,7 @@ const StartGameScreen = (props) => {
 
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
+      <TitleText style={styles.title}>Start a New Game!</TitleText>
       <Card style={styles.inputContainer}>
         <BodyText>Select a number:</BodyText>
         <Input
