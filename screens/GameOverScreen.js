781e461e1a320e9667da1796b6a55bba65e6f77f
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-12-16 11:59:53 +0100

    refactor: move Button import to be with other react-native imports

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button } from "react-native";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = (props) => {
   return (
